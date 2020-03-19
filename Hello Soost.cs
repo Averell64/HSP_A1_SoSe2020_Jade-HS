801f35ff552038f8d4cb278b596706cf10d59259
Averell64 <47695923+Averell64@users.noreply.github.com>
2020-03-19 08:57:41 +0100

    Create Hello Soost.cs

---
+++ Hello Soost.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*
+ Hello World Programm
+ Version 1.0
+ letzte Aenderung 17.09.2019
+ Kai Mecke
+*/
+
+namespace Hello_World_Mecke
+{
+    class Program
+    {
+        static void Main(string[] args) // Hier beginnt das Hauptprogramm 
+        {
+            // Ausgabe Benutzer 1
+            Console.WriteLine("Hello World von XXX");
+
+            // Ausgabe Benutzer 2
+            Console.WriteLine("Nǐ hǎo, shìjiè");
+
+            // Ausgabe Benutzer 3
+            Console.WriteLine("Hallo Welt von XXX");
+
+            // Ausgabe Benutzer 4
+            Console.WriteLine("marhabaan bialealam");
+
+            // warte bis der User eine Taste drueckt
+            Console.ReadKey();
+
+        } // Hier endet das Hauptprogramm
+    }
+}
